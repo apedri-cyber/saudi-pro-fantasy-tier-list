44ce6de75f55cbe5488bf9d503346faad4ae0e8c
Alex Pedri <157630286+apedri-cyber@users.noreply.github.com>
2026-09-15 13:10:37 -0400

    Mount commissioner moderation controls

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
+import CommissionerModeration from "./CommissionerModeration.jsx";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode><App /></React.StrictMode>
+  <React.StrictMode>
+    <App />
+    <CommissionerModeration />
+  </React.StrictMode>
 );
